5a955ff8e6546a5bd763f9cedae2583cc16710b4
sakila sandanuwan <75413267+saki99729@users.noreply.github.com>
2025-09-15 10:17:33 +0530

    Enhance GET / test to include status code check
    Updated test to check status code in response.

--- test/test.js
+++ test/test.js
@@ -1,8 +1,11 @@
-var request = require('supertest');
-var app = require('../index.js');
+const request = require('supertest');
+const app = require('../index.js'); // just the app, no server.listen()
 
 describe('GET /', function () {
-    it('respond with hello world', function (done) {
-        request(app).get('/').expect('Hello World!', done);
-    });
+  it('respond with hello world', function (done) {
+    request(app)
+      .get('/')
+      .expect(200) // check status code as well
+      .expect('Hello World!', done);
+  });
 });
